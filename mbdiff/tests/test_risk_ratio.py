f643b643811f218104ec83b9bf3e917376be6d8c
Piotr Zakrzewski <1050269+PiotrZakrzewski@users.noreply.github.com>
2020-11-14 18:13:46 +0100

    Risk ratio

--- mbdiff/tests/test_risk_ratio.py
+++ mbdiff/tests/test_risk_ratio.py
@@ -1,4 +1,4 @@
-from mbdiff.risk_ratio import calc_support
+from mbdiff.risk_ratio import calc_support, risk_ratio
 import pytest
 from pandas import DataFrame, Series
 
@@ -29,8 +29,50 @@ def df_basic(int_column, float_column, cat_col_all_same, outlier_col):
         'outlier': outlier_col,
     })
 
-def test_risk_basic():
-    pass
+@pytest.fixture
+def df_outliers(int_column, float_column, cat_col_all_same, outlier_col):
+    outlier_col[9] = "outlier"
+    float_column[9] = 999.8
+    cat_col_all_same[9] = "cat2" 
+    return DataFrame({
+        'ints': int_column,
+        'floats':float_column,
+        'cats': cat_col_all_same,
+        'outlier': outlier_col,
+    })
+
+@pytest.fixture
+def df_outliers_balanced(int_column, float_column, cat_col_all_same, outlier_col):
+    outlier_col[9] = "outlier"
+    outlier_col[8] = "outlier"
+    float_column[9] = 999.7
+    float_column[8] = 999.8
+    cat_col_all_same[9] = "cat2" 
+    cat_col_all_same[8] = "cat1" 
+    cat_col_all_same[7] = "cat2" 
+    return DataFrame({
+        'ints': int_column,
+        'floats':float_column,
+        'cats': cat_col_all_same,
+        'outlier': outlier_col,
+    })
+
+def test_risk_basic_null_1(df_outliers):
+    """Due to lack of cat2 in the inliers the result will be 0.0."""
+    comb = {'cats':'cat2'}
+    assert risk_ratio(comb, df_outliers) == pytest.approx(0.0)
+
+def test_risk_basic_null_2(df_outliers):
+    """Due to lack of cat1 in the outliers the result will be 0.0."""
+    comb = {'cats':'cat1'}
+    assert risk_ratio(comb, df_outliers) == pytest.approx(0.0)
+
+def test_risk_basic_balanced(df_outliers_balanced):
+    """Due to lack of cat1 in the outliers the result will be 0.0."""
+    comb = {'cats':'cat2'}
+    assert risk_ratio(comb, df_outliers_balanced) == pytest.approx(4.0)
+    comb = {'cats':'cat1'}
+    assert risk_ratio(comb, df_outliers_balanced) == pytest.approx(0.25)
 
 def test_support(df_basic):
     assert calc_support(df_basic, 'cats', 'cat1') == pytest.approx(1.0)
