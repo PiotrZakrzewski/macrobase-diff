8af44709814bdfe1243df86a31cf995847818515
Piotr Zakrzewski <1050269+PiotrZakrzewski@users.noreply.github.com>
2020-12-03 08:52:14 +0100

    Remove unused naive implementation for attr mining

--- mbdiff/diff.py
+++ mbdiff/diff.py
@@ -2,7 +2,6 @@ from typing import List
 from pandas import DataFrame, read_csv
 from mbdiff.diff_query import DiffQuery, InvalidQuery
 from mbdiff.risk_ratio import risk_ratio
-from mbdiff.attribute_mining import get_combs
 from numpy import nan
 from mbdiff.apriori import explain as aprio_explain
 
@@ -35,10 +34,10 @@ def diff(
             ignored_cols.append(column)
     sel_cols = [col for col in df.columns if col not in ignored_cols]
     df = df[sel_cols]
-    return _apriori_diff(df, max_order, min_support, min_risk, ignored_cols)
+    return _apriori_diff(df, max_order, min_support, min_risk)
 
 
-def _apriori_diff(df: DataFrame, max_order: int, min_support: float, min_risk: float, ignored_cols: List):
+def _apriori_diff(df: DataFrame, max_order: int, min_support: float, min_risk: float):
     """Explanation mining from attributes close to macrobase DIFF, with use of Apriori."""
     explanations = aprio_explain(df, min_support, max_order)
     results, invalid = [], []
@@ -49,19 +48,4 @@ def _apriori_diff(df: DataFrame, max_order: int, min_support: float, min_risk: f
             invalid.append(explanation)
         else:
             results.append(res)
-    return results, invalid
-
-
-def _naive_impl(df: DataFrame, max_order: int, min_support: float, min_risk: float, ignored_cols: List):
-    """My own naive implementation of attribute mining, akin to apriori but without minimization."""
-    combinations = get_combs(df, max_order, min_support, ignored_cols)
-    results, invalid = [], []
-    for combination in combinations:
-        rr = risk_ratio(combination, df)
-        res = (rr, combination)
-        if rr is nan or rr <= min_risk:
-            invalid.append(combination)
-        else:
-            results.append(res)
-
     return results, invalid
